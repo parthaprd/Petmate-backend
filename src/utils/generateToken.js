87f7b25e777f2503649625abf7947445f053d1c4
parthaprd <parthapratimd16@gmail.com>
2026-05-18 13:20:25 +0600

    Add utility functions for token generation

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,10 +1,9 @@
-// JWT token generator
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-module.exports = function (user) {
-  return jwt.sign(
-    { id: user._id, username: user.username, role: user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: '1d' }
-  );
-};
+function generateToken(payload) {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
+
+module.exports = generateToken;
