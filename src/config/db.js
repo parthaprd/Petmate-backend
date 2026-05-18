04819bac795c13d1ee253a3720e2f22852f4fee4
parthaprd <parthapratimd16@gmail.com>
2026-05-18 13:20:02 +0600

    Add database configuration module

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,13 @@
-// MongoDB connection setup
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
 
-const connectDB = async () => {
+async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB Connected");
   } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   }
-};
+}
 
 module.exports = connectDB;
