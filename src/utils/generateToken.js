ece5eda834f6353f956599e42b1dd435b9cd1aeb
parthaprd <parthapratimd16@gmail.com>
2026-05-21 00:36:21 +0600

    login,register issue fixed

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 function generateToken(payload) {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+  return jwt.sign(payload, process.env.JWT_SECRET || "fallback_secret_key_for_assignment", {
+    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
   });
 }
 
